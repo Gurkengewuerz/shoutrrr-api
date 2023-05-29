3b3a5fdf4327f01ed859ca39dbcebe2f66372baf
Gurkengewuerz <niklas@schuetrumpf.net>
2023-05-29 13:29:28 +0200

    feat: added more logging options

--- main.go
+++ main.go
@@ -13,14 +13,18 @@ import (
 )
 
 type Config struct {
-	Port   int `yaml:"port"`
-	Config []struct {
+	Port     int `yaml:"port"`
+	LogLevel int `yaml:"logLevel"`
+	Config   []struct {
 		ID    string   `yaml:"id"`
 		Shout []string `yaml:"shout"`
 	} `yaml:"config"`
 }
 
+var config = Config{}
+
 func ParseError(c *fiber.Ctx) error {
+	log.Warnf("failed to parse request")
 	return c.Status(fiber.StatusBadRequest).JSON(map[string]interface{}{
 		"error":   true,
 		"message": "failed to parse request",
@@ -32,9 +36,7 @@ func main() {
 		DisableColors: false,
 		FullTimestamp: true,
 	})
-	log.SetLevel(log.InfoLevel)
-
-	config := Config{}
+	log.SetLevel(log.DebugLevel)
 
 	dat, err := os.ReadFile("config.yml")
 	if err != nil {
@@ -46,11 +48,13 @@ func main() {
 		panic(err)
 	}
 
+	log.SetLevel(log.AllLevels[config.LogLevel])
+
 	for _, entry := range config.Config {
 		for _, url := range entry.Shout {
 			_, err := shoutrrr.CreateSender(url)
 			if err != nil {
-				panic(err)
+				log.WithError(err).WithField("url", url).Panic("failed to parse shoutrrr url")
 			}
 		}
 	}
@@ -69,6 +73,7 @@ func main() {
 			}
 		}
 		if len(shout) == 0 {
+			log.WithField("id", c.Params("id")).Warnf("unknown id request")
 			return c.Status(fiber.StatusForbidden).JSON(map[string]interface{}{
 				"error":   true,
 				"message": "unknown id",
@@ -79,8 +84,10 @@ func main() {
 		case "slack":
 			req := new(types.Payload)
 			if err := c.BodyParser(req); err != nil {
+				log.Warnf("unknown slack body")
 				return ParseError(c)
 			}
+			log.WithField("req", req).Debug("new slack request")
 			msg = req.Text
 			for _, attachment := range req.Attachments {
 				if len(msg) == 0 {
@@ -107,8 +114,10 @@ func main() {
 		case "discord":
 			req := new(types.Message)
 			if err := c.BodyParser(req); err != nil {
+				log.Warnf("unknown discord body")
 				return ParseError(c)
 			}
+			log.WithField("req", req).Debug("new discord request")
 			msg = req.Content
 			for _, embed := range req.Embeds {
 				if len(msg) == 0 {
@@ -142,14 +151,17 @@ func main() {
 		default:
 			req := new(types.Simple)
 			if err := c.BodyParser(req); err != nil {
+				log.Warnf("unknown default body")
 				return ParseError(c)
 			}
+			log.WithField("req", req).Debug("new default request")
 			title = req.Title
 			msg = req.Message
 		}
 
 		sender, err := shoutrrr.CreateSender(shout...)
 		if err != nil {
+			log.WithError(err).Warnf("failed to create shoutrrr sender")
 			return c.Status(fiber.StatusBadRequest).JSON(map[string]interface{}{
 				"error":   true,
 				"message": err.Error(),
@@ -167,12 +179,14 @@ func main() {
 			errorString = append(errorString, theError.Error())
 		}
 		if len(errorString) > 0 {
+			log.WithField("errors", errorString).Warnf("failed to send shoutrrr message")
 			return c.Status(fiber.StatusBadRequest).JSON(map[string]interface{}{
 				"error":   true,
 				"message": errorString,
 			})
 		}
 
+		log.WithField("id", c.Params("id")).Debug("new shot send")
 		return c.Status(fiber.StatusCreated).JSON(map[string]interface{}{
 			"error":   false,
 			"message": "ok",
